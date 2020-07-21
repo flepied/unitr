90ee325b0ae4c60b35330eaa7eea57b851ff4bd2
Frederic Lepied <flepied@gmail.com>
2020-07-21 22:54:54 +0200

    support branch based checking

--- verify-patch.sh
+++ verify-patch.sh
@@ -23,43 +23,73 @@ success() {
 
 failure() {
     echo "Failure: $*"
-    echo "https://review.openstack.org/#/c/$review/"
+    if [ $gerrit -eq 1 ]; then
+        echo "https://review.openstack.org/#/c/$review/"
+    fi
     exit 1
 }
 
 cleanup() {
     git checkout .
     git clean -d -f > /dev/null
-    git branch -D $branch
+    if [ $gerrit -eq 1 ]; then
+        git branch -D $branch
+    fi
 }
 
 set -e
 
-if [ $# != 4 ]; then
-    echo "Usage: $0 <review id> <review number> <git basedir> <log dir>" 1>&2
-    exit 1
-fi
+case $# in
+    4)
+        gerrit=1
+        ;;
+    3)
+        gerrit=0
+        ;;
+    *)
+        echo "Usage: $0 <review id> <review number> <git basedir> <log dir>" 1>&2
+        echo "Usage: $0 <branch name> <git basedir> <log dir>" 1>&2
+        exit 1
+        ;;
+esac
 
 if ! type -p filterdiff > /dev/null 2>&1; then
     echo "Install filterdiff" 1>&2
     exit 1
 fi
 
-review="$1"
-number="$2"
-BASEDIR="$3"
-LOGDIR="$4"
+if [ $gerrit -eq 1 ]; then
+    review="$1"
+    number="$2"
+    BASEDIR="$3"
+    LOGDIR="$4"
+    log=$LOGDIR/$review.$number.tests.txt
+else
+    branch="$1"
+    BASEDIR="$2"
+    LOGDIR="$3"
+    log=$LOGDIR/$(echo $branch|sed 's@/@_@g').tests.txt
+fi
 
 cd $BASEDIR
 
-git review -d "$review,$number"
-rev=$(git rev-parse HEAD)
-branch=$(git rev-parse --abbrev-ref HEAD)
+if [ $gerrit -eq 1 ]; then
+    git review -d "$review,$number"
+    rev=$(git rev-parse HEAD)
+    branch=$(git rev-parse --abbrev-ref HEAD)
 
-echo "Processing $rev pointed by $branch"
+    echo "Processing $rev pointed by $branch"
 
-git checkout HEAD^ > /dev/null 2>&1
-git show $rev  > diff.$$
+    git checkout HEAD^ > /dev/null 2>&1
+    git show $rev  > diff.$$
+else
+    git fetch
+    git checkout $branch
+    git diff -r master > diff.$$
+    git checkout master
+    git pull
+fi
+
 filterdiff -i '*/test*' < diff.$$ > fdiff.$$
 trap cleanup 0
 
@@ -78,13 +108,23 @@ if [ -z "$(sed -n -e 's/^+++ //p' < diff.$$ | egrep -v '.*/test.*\.py')" ]; then
     success "Only tests, nothing to check"
 fi
 
+# find the right python env
+if grep -q py37 tox.ini; then
+    runner="tox -epy37"
+else
+    runner="tox -epy27"
+fi
+
 # Apply the filtered diff with only tests
-patch -p1 < fdiff.$$
+if [ $gerrit -eq 1 ]; then
+    patch -p1 < fdiff.$$
+else
+    patch -p0 < fdiff.$$
+fi
 
 # We have code so see if at least one test fails without the code
 fail=0
-log=$LOGDIR/$review.$number.tests.txt
-runner="tox -epy27"
+
 for tfile in $(sed -n -e 's@^+++ \([ab]/\)\?@@p' < fdiff.$$ | egrep '.*/test.*\.py'); do
     testname=$(echo "$tfile" | sed -e 's@.*/tests/@@' -e 's@.py@@' -e 's@/@.@g')
     echo "+ Running $runner $testname"
